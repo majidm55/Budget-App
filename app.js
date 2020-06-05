ca81c02ab2e70a364b2005880e0c89405baf526a
Majid <mohammadmajid55@gmail.com>
2020-06-04 20:29:08 -0400

    getting input data and setting put UI and global controllers

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
+
+//BUDGET CONTROLLER
 var budgetController = (function() {
-
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
-})();
-
-
-var UIcontroller = (function() {
  // some code
 
+    
 })();
 
-var controller = (function(budgetCtrl, UIctrl) {
+// UI CONTROLLER
+var UIcontroller = (function() {
 
-    var z = budgetController.publicTest(5);
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+
     }
+    return {
+        getInput: function() {
+
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,  // will be either inc or exp  
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };      
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UIctrl) {
+    
+    var DOM = UIctrl.getDOMstrings();
+
+    var ctrlAddItem = function() {
+        // 1. Get field input data
+        var input = UIcontroller.getInput();
+        console.log('input', input);
+        // 2. Add item to the budget controller
+        //3.  Add Item to UI
+        //4.  Calculate the budget
+        //5. Display the budget on the UI 
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
 })(budgetController, UIcontroller);
+
+
+
