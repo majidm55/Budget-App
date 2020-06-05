c6060e70d58eb305acea71ea82145f51e5665e4b
Majid <mohammadmajid55@gmail.com>
2020-06-04 23:07:34 -0400

    adding a data structure then, new item and id to the budget, pushing the new item to the appropriate set

--- app.js
+++ app.js
@@ -25,6 +25,32 @@ var budgetController = (function() {
             inc: 0,
         }
         
+    };
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID 
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp'
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            //Push item into data structure
+            data.allItems[type].push(newItem);
+            // Return new item
+            return newItem;
+        },
+        testing: function(){
+            console.log('data structure', data);
+        }
     }
     
 })();
@@ -75,10 +101,12 @@ var controller = (function(budgetCtrl, UIctrl) {
     
 
     var ctrlAddItem = function() {
-
+        var input, newItem;
         // 1. Get field input data
-        var input = UIcontroller.getInput();
+        input = UIcontroller.getInput();
+
         // 2. Add item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //3.  Add Item to UI
         //4.  Calculate the budget
         //5. Display the budget on the UI 
